7cfdfe7c42eb95ed33884cb691db607c314912ce
farxa <r.abualhassan@gmail.com>
2024-04-25 19:52:18 +0200

    create react app and configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [tailwindcss, autoprefixer],
+};
