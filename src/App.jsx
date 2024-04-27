4a61158e67012a62df97e5e551477bc73bc835bd
farxa <r.abualhassan@gmail.com>
2024-04-27 13:47:07 +0200

    create header to render logo. install dinerojs to handle price formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
       <div>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from "../assets/logo.svg";
+
+const Header = () => {
+  return (
+    <header className="mx-5 my-1">
+      <img src={logo} alt="Logo" />
+    </header>
+  );
+};
+
+export default Header;
